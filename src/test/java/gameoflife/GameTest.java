c35e5ce4cb909fbec4df9b5ef5db16743d5e10a1
Ola Lindgård <ola.lindgard@iths.se>
2021-12-20 09:17:03 +0100

    Initiated project with test file and matrix file.

---
+++ src/test/java/gameoflife/GameTest.java
@@ -0,0 +1,12 @@
+package gameoflife;
+
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @Test
+    public void foundOneLivingCellInMatrix() {
+
+        Matrix matrix = new Matrix();
+    }
+}
